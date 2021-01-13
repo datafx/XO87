6a9aea9953e45a4a09ad3b2e000ba9cdf1c1853d
datafx <2432997+datafx@users.noreply.github.com>
2021-01-12 23:20:29 -0500

    corrected license and cleaned up some things

--- xo87/rgb/rgb.h
+++ xo87/rgb/rgb.h
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
 #define u8 uint8_t
 #define u16 uint16_t
 
