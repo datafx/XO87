d3512a5c3bfce90c162071cf3a7fe60abfe095d2
datafx <2432997+datafx@users.noreply.github.com>
2021-01-17 13:45:27 -0500

    Cleanup rules.mk

--- xo87/rgb/rules.mk
+++ xo87/rgb/rules.mk
@@ -1,14 +1,6 @@
 # MCU name
 MCU = at90usb646
 
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = lufa-dfu
 
 # Build Options
@@ -24,12 +16,9 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 RAW_ENABLE = no
